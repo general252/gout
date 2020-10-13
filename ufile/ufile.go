449a693f98d36d84442e16336bec43ae5d3f703f
lcl <475807132@qq.com>
2020-10-13 17:03:02 +0800

    add file

---
+++ ufile/ufile.go
@@ -0,0 +1,71 @@
+package ufile
+
+import (
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"fmt"
+	"io"
+	"os"
+)
+
+// MD5
+func Md5File(path string) (string, error) {
+	file, err := os.Open(path)
+	defer file.Close()
+	if err != nil {
+		return "", err
+	}
+
+	w := md5.New()
+	_, err = io.Copy(w, file)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x", w.Sum(nil)), nil
+}
+
+// SHA1
+func SHA1File(path string) (string, error) {
+	file, err := os.Open(path)
+	defer file.Close()
+	if err != nil {
+		return "", err
+	}
+
+	h := sha1.New()
+	_, err = io.Copy(h, file)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%x", h.Sum(nil)), nil
+}
+
+// SHA256
+func SHA256File(path string) (string, error) {
+	file, err := os.Open(path)
+	defer file.Close()
+	if err != nil {
+		return "", err
+	}
+
+	h := sha256.New()
+	_, err = io.Copy(h, file)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%x", h.Sum(nil)), nil
+}
+
+// 判断所给路径文件/文件夹是否存在
+func IsExists(path string) bool {
+	_, err := os.Stat(path) //os.Stat获取文件信息
+	if err != nil {
+		if os.IsExist(err) {
+			return true
+		}
+		return false
+	}
+	return true
+}
