6481ca46b7e9033eb3ade01a6b77df3012f85d26
lcl <475807132@qq.com>
2020-11-04 12:06:47 +0800

    add get mac

--- unet/unet.go
+++ unet/unet.go
@@ -1,6 +1,7 @@
 package unet
 
 import (
+	"fmt"
 	"net"
 	"strings"
 )
@@ -17,4 +18,34 @@ func GetHostIP() (string, error) {
 
 	var ip = strings.Split(conn.LocalAddr().String(), ":")[0]
 	return ip, nil
+}
+
+// GetMacAddress 获取MAC地址
+func GetMacAddress() (string, error) {
+	var ip, err = GetHostIP()
+	if err != nil {
+		return "", err
+	}
+	interFaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+	var findInterFace = func(ip string) (*net.Interface, error) {
+		for _, face := range interFaces {
+			if addrList, err := face.Addrs(); err == nil {
+				for _, addr := range addrList {
+					if strings.HasPrefix(addr.String(), ip) {
+						return &face, nil
+					}
+				}
+			}
+		}
+		return nil, fmt.Errorf("not found")
+	}
+
+	if face, err := findInterFace(ip); err != nil {
+		return "", err
+	} else {
+		return face.HardwareAddr.String(), nil
+	}
 }
