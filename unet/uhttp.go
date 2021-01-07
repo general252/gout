27e0aeded8c5163f1931bc2f110c8a3f65c513f2
tony <tony@gmail.com>
2021-01-07 11:25:13 +0800

    http util

--- unet/uhttp.go
+++ unet/uhttp.go
@@ -1,7 +1,10 @@
 package unet
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"io"
 	"io/ioutil"
 	"net"
 	"net/http"
@@ -9,20 +12,7 @@ import (
 	"time"
 )
 
-/** http 请求
-data, _ := HttpDo(
-	"POST",
-	"http://127.0.0.1/api/test/authorization",
-	"{\"a\":1,\"b\":\"22\"}",
-	map[string]string{
-		"Content-Type":  "application/json; charset=utf-8",
-		"Authorization": "abc|20C2FB99644A789BE386722EEFF37AE7D062866B",
-		"Content-MD5":   "MzExNUJDMTkzMjIxMTJCMw==",
-		"Date":          "Tue, 15 Nov 1994 08:12:31 GMT",
-	})
-
-fmt.Printf("%v\n", string(data))
-*/
+// HttpDo
 func HttpDo(method, url, data string, headers map[string]string) ([]byte, error) {
 	client := &http.Client{
 		Transport: &http.Transport{
@@ -66,4 +56,72 @@ func HttpDo(method, url, data string, headers map[string]string) ([]byte, error)
 	}
 
 	return body, nil
+}
+
+// HttpRequestJson http请求, context-type: "application/json; charset=utf-8"
+func HttpRequestJson(method string, url string, body *bytes.Buffer) ([]byte, error) {
+	return HttpRequestWithContextType(method, url, body, "application/json; charset=utf-8", nil)
+}
+
+// HttpRequestMultipartFormData http请求, context-type: "multipart/form-data"
+func HttpRequestMultipartFormData(method string, url string, body *bytes.Buffer) ([]byte, error) {
+	return HttpRequestWithContextType(method, url, body, "multipart/form-data", nil)
+}
+
+// HttpRequestWithoutContextType http请求, no context-type
+func HttpRequestWithoutContextType(method string, url string, body *bytes.Buffer) ([]byte, error) {
+	return HttpRequestWithContextType(method, url, body, "", nil)
+}
+
+// HttpRequestWithContextType http请求
+func HttpRequestWithContextType(method string, url string, body *bytes.Buffer, contextType string, headers map[string]string) ([]byte, error) {
+	var p io.Reader
+	if body != nil {
+		p = body
+	}
+	req, err := http.NewRequest(method, url, p)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(contextType) > 0 {
+		req.Header.Add("Content-Type", contextType)
+	}
+
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
+
+	cli := &http.Client{
+		Transport: &http.Transport{
+			DialContext: func(ctx context.Context, network, addr string) (conn net.Conn, e error) {
+				conn, err := net.DialTimeout(network, addr, time.Second*5) //设置建立连接超时
+				if err != nil {
+					return nil, err
+				}
+				return conn, nil
+			},
+		},
+	}
+
+	resp, err := cli.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return ioutil.ReadAll(resp.Body)
+}
+
+func FormatJson(jsonData []byte) []byte {
+	var obj interface{}
+	if err := json.Unmarshal(jsonData, &obj); err != nil {
+		return nil
+	}
+
+	if data, err := json.MarshalIndent(obj, "", "  "); err != nil {
+		return nil
+	} else {
+		return data
+	}
 }
---
+++ unet/uhttp_test.go
@@ -0,0 +1,93 @@
+package unet
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"path/filepath"
+)
+
+var (
+	BaseURL = "http://127.0.0.1/api/test"
+)
+
+func ExampleHttpDo() {
+	data, _ := HttpDo(
+		"POST",
+		fmt.Sprintf("%v/authorization", BaseURL),
+		"{\"a\":1,\"b\":\"22\"}",
+		map[string]string{
+			"Content-Type":  "application/json; charset=utf-8",
+			"Authorization": "abc|20C2FB99644A789BE386722EEFF37AE7D062866B",
+			"Content-MD5":   "MzExNUJDMTkzMjIxMTJCMw==",
+			"Date":          "Tue, 15 Nov 1994 08:12:31 GMT",
+		})
+
+	fmt.Printf("%v\n", string(data))
+	// output:
+	//
+}
+
+func ExampleHttpRequestJson() {
+	url := fmt.Sprintf("%v/file/%v", BaseURL, fileId)
+	resp, err := HttpRequestJson(http.MethodGet, url, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Println(string(FormatJson(resp)))
+	// output:
+	//
+}
+
+// 上传文件
+func ExampleHttpRequestWithContextType() {
+	url := fmt.Sprintf("%v/file", BaseURL)
+	path := `/your/file/path`
+
+	body, contextType, err := getPostFileBody("uploadFile", path)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	resp, err := HttpRequestWithContextType(http.MethodPost, url, body, contextType, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Println(string(FormatJson(resp)))
+
+	// output:
+	//
+
+}
+
+// getPostFileBody 获取上传文件的body, context-type
+func getPostFileBody(uploadFileKey string, fileFullPath string) (*bytes.Buffer, string, error) {
+	fileData, err := ioutil.ReadFile(fileFullPath)
+	if err != nil {
+		return nil, "", err
+	}
+
+	var resp bytes.Buffer
+
+	// 文件写入 body
+	writer := multipart.NewWriter(&resp)
+	defer writer.Close()
+
+	part, err := writer.CreateFormFile(uploadFileKey, filepath.Base(fileFullPath))
+	if err != nil {
+		return nil, "", err
+	}
+
+	_, _ = part.Write(fileData)
+
+	return &resp, writer.FormDataContentType(), nil
+}
