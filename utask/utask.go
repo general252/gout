7b3c1d7fd1e6fb73b9662a904e31307ab3948c8e
tony <tony@gmail.com>
2021-01-27 14:53:54 +0800

    add mutex

--- utask/utask.go
+++ utask/utask.go
@@ -4,15 +4,20 @@ import (
 	"fmt"
 	"github.com/robfig/cron/v3"
 	"log"
+	"sync"
 	"time"
 )
 
 var (
 	cronTab *cron.Cron = nil
+	mux     sync.Mutex
 )
 
 // AddTaskEvery 添加任务. id: 任务标识
 func AddTaskEvery(everyDuration time.Duration, cmd func()) (int, error) {
+	mux.Lock()
+	defer mux.Unlock()
+
 	if cronTab == nil {
 		cronTab = cron.New(cron.WithSeconds())
 		cronTab.Start()
@@ -26,6 +31,9 @@ func AddTaskEvery(everyDuration time.Duration, cmd func()) (int, error) {
 
 // RemoveTask 删除任务
 func RemoveTask(id int) {
+	mux.Lock()
+	defer mux.Unlock()
+
 	if cronTab == nil {
 		return
 	}
@@ -35,6 +43,9 @@ func RemoveTask(id int) {
 
 // StopCron 关闭任务调度
 func StopCron() {
+	mux.Lock()
+	defer mux.Unlock()
+
 	if cronTab != nil {
 		ctx := cronTab.Stop()
 		select {
