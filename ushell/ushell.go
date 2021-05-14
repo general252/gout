96656c718b8883685707f1e193add2de27feca5d
tony <tony@gmail.com>
2021-05-14 17:27:08 +0800

    update

--- ushell/ushell.go
+++ ushell/ushell.go
@@ -1,12 +1,14 @@
 package ushell
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"github.com/axgle/mahonia"
 	"os/exec"
 	"runtime"
 	"strings"
+	"sync"
 )
 
 // ShellCommand shell
@@ -35,6 +37,61 @@ func ShellCommand(command string) (string, string, error) {
 	strErr = dec.ConvertString(strErr)
 
 	return strOut, strErr, err
+}
+
+// ShellCommandStream("ls -ltr", func([]byte, bool){ })  isError: true是stderr输出, false: 是stdout输出
+// ShellCommandStream("SystemInfo", func([]byte, bool){ })
+func ShellCommandStream(command string, cb func(msg []byte, isError bool)) error {
+	if cb == nil {
+		return fmt.Errorf("cb is nil")
+	}
+
+	var cmd *exec.Cmd
+
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd", "/C", command)
+	} else {
+		cmd = exec.Command("sh", "-c", command)
+	}
+
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return err
+	}
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+
+	wg := sync.WaitGroup{}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		scanner := bufio.NewScanner(stdout)
+		for scanner.Scan() {
+			cb(scanner.Bytes(), false)
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		scanner := bufio.NewScanner(stderr)
+		for scanner.Scan() {
+			cb(scanner.Bytes(), true)
+		}
+	}()
+
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	wg.Wait()
+
+	return err
 }
 
 // OpenUri open uri on browser
